90df82f47448601e2bd0d523ed82562f57610636
Kasu-yarou <zeal24360192@fsgcl.jp>
2025-05-26 10:05:49 +0900

    setColorの追加

--- src/初級Java/RectTest.java
+++ src/初級Java/RectTest.java
@@ -2,11 +2,24 @@ package 初級Java;
 
 public class RectTest extends MyFrame {
 	public void run() {
-
+		setColor(255, 255, 0);
 		fillRect(30, 40, 50, 100);
+
+		setColor(255, 0, 255);
+		fillRect(130, 40, 50, 100);
+
+		setColor(0, 255, 255);
+		fillRect(230, 40, 50, 100);
+
+		setColor(0, 0, 0);
+		fillRect(330, 40, 50, 100);
+
+		setColor(100, 100, 100);
+		fillRect(360, 40, 50, 100);
 
 		for (int j = 0; j <= 10; j++) {
 			for (int i = 0; i <= 10; i++) {
+				setColor(255 - (i * 10), 255 - (i * 10), 255 - (i * 10));
 				fillRect(90 + (i * 35), 160 + (j * 35), 30, 30);
 			}
 		}
