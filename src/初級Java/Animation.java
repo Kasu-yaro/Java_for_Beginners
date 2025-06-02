9cbf0d80cf6ebb7688788a94bdbf1dfd7d580f28
Kasu-yarou <zeal24360192@fsgcl.jp>
2025-06-02 10:41:13 +0900

    課題EXまで-済

--- src/初級Java/Animation.java
+++ src/初級Java/Animation.java
@@ -1,12 +1,15 @@
 package 初級Java;
 
 public class Animation extends MyFrame {
-	
+
 	int z;
 	int w;
-	
-	public void run() {
+	int z_up;
+	int w_up;
+	int z_Henkan;
+	boolean z_if = true;
 
+	public void run() {
 
 		for (int j = 0; j <= 10; j++) {
 			//Ani0();	
@@ -15,8 +18,7 @@ public class Animation extends MyFrame {
 			//Ani3();
 			//AniEX1();
 			//AniEX2();
-			AniEX2_1();
-
+			//AniEX2_1();//ループ文3つ
 
 		}
 
@@ -103,14 +105,13 @@ public class Animation extends MyFrame {
 			setColor(0, 128, 0);
 			//fillRect(x * x / 20, 80, 10, 100);
 			fillOval(x * x / 20, y * y / 20, 100, 100);
-			
-			x+=1;
-			y-=1;
+
+			x += 1;
+			y -= 1;
 			sleep(0.018);
 
-			
 		}
-		
+
 		while (x > 0) {
 			//画面を消す
 			clear();
@@ -118,13 +119,12 @@ public class Animation extends MyFrame {
 			setColor(0, 128, 0);
 			//fillRect(x * x / 20, 80, 10, 100);
 			fillOval(x * x / 20, y * y / 20, 100, 100);
-			
-			x-=1;
+
+			x -= 1;
 			sleep(0.018);
 
-			
 		}
-		
+
 		while (x < 100) {
 			//画面を消す
 			clear();
@@ -132,19 +132,20 @@ public class Animation extends MyFrame {
 			setColor(0, 128, 0);
 			//fillRect(x * x / 20, 80, 10, 100);
 			fillOval(x * x / 20, y * y / 20, 100, 100);
-			
-			x-=1;
-			y+=1;
+
+			x -= 1;
+			y += 1;
 			sleep(0.018);
 
-			
 		}
-		
+
 	}
+
 	private void Ani3() {
 		//Ani0参照
-		
+
 	}
+
 	private void AniEX1() {
 		int x = 0;
 		int y = 0;
@@ -173,7 +174,7 @@ public class Animation extends MyFrame {
 			sleep(0.018);
 
 		}
-		
+
 		while (x > 0) {
 			//画面を消す
 			clear();
@@ -186,7 +187,7 @@ public class Animation extends MyFrame {
 			sleep(0.018);
 
 		}
-		
+
 		while (y > 0) {
 			//画面を消す
 			clear();
@@ -199,11 +200,9 @@ public class Animation extends MyFrame {
 			sleep(0.018);
 
 		}
-		
-		
-		
-		
+
 	}
+
 	private void AniEX2() {
 		int x = 0;
 		int y = 0;
@@ -232,7 +231,7 @@ public class Animation extends MyFrame {
 			sleep(0.018);
 
 		}
-		
+
 		while (x > 0) {
 			//画面を消す
 			clear();
@@ -257,23 +256,36 @@ public class Animation extends MyFrame {
 			sleep(0.018);
 
 		}
-		
-		
-		
-		
+
 	}
+
 	private void AniEX2_1() {
+		z_if = true;
 		z = 0;
 		w = 0;
+		z_Henkan = 1;
+
+		z_up = 75;
+		w_up = 75;
 
 		move_X();
-	
+
+		move_Y();
+
+		z_if = false;
+		z_Henkan = -1;
+
+		z_up = 0;
+		w_up = 100;
+
+		move_X();
+
 		move_Y();
 
 	}
-	
+
 	private void move_X() {
-		while (z < 100) {
+		while (z * z_Henkan < z_up * z_Henkan) {
 			//画面を消す
 			clear();
 			//	四角形を表示する
@@ -281,13 +293,18 @@ public class Animation extends MyFrame {
 			//fillRect(x * x / 20, 80, 10, 100);
 			fillOval(z * z / 20, w * w / 20, 100, 100);
 
-			z += 1;
+			if (z_if == true) {
+				z += 1;
+			} else {
+				z -= 1;
+			}
+
 			sleep(0.018);
 		}
 	}
-	
+
 	private void move_Y() {
-		while (w < 100) {
+		while (w < w_up) {
 			//画面を消す
 			clear();
 			//	四角形を表示する
