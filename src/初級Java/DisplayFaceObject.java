ca5b5e42655753dbf11f6dc0843a3dcfe1ac08f2
Kasu-yarou <zeal24360192@fsgcl.jp>
2025-06-03 10:05:51 +0900

    課題(車 ねじれ)

--- src/初級Java/DisplayFaceObject.java
+++ src/初級Java/DisplayFaceObject.java
@@ -2,18 +2,22 @@ package 初級Java;
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1 = new Face(50, 50,10,5);
-		Face face2 = new Face(200, 100,-10,-5);
+		Face face1 = new Face(-100, 50, 50, 0);
+		Face face2 = new Face(1200, 100, -50, 0);
 
-		for (int i = 0; i <= 30; i++) {
-			clear();
-			face1.draw(this);
-			face2.draw(this);
+		for (int j = 0; j <= 100; j++) {
+			face1.x = -100;
+			face2.x = 1200;
+			for (int i = 0; i <= 40; i++) {
+				clear();
+				face1.draw(this);
+				face2.draw(this);
 
-			face1.move();
-			face2.move();
+				face1.move();
+				face2.move();
 
-			sleep(0.018);
+				sleep(0.009);
+			}
 		}
 
 	}
--- src/初級Java/Face.java
+++ src/初級Java/Face.java
@@ -11,9 +11,15 @@ public class Face {
 	}
 
 	public void draw(MyFrame frame) {
-		frame.fillOval(x, y, 50, 100);
-		frame.fillOval(x + 100, y, 50, 100);
-		frame.fillRect(x, y + 160, 150, 10);
+		//frame.fillOval(x, y, 50, 100);
+		//frame.fillOval(x + 100, y, 50, 100);
+		//frame.fillRect(x, y + 160, 150, 10);
+		
+		frame.fillRect(x+100, y+100, 75, 30);
+		frame.fillRect(x+60, y+130, 150, 30);
+		frame.fillOval(x, y+160, 120, 120);
+		frame.fillOval(x+150, y+160, 120, 120);
+		
 	}
 
 	public void move() {
